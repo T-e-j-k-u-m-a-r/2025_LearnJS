09aa3fe39c44dc2fd3ee5c78d3d98c25a0d193e0
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-05-26 14:52:12 +0530

    Added operators_part1 file

---
+++ 01_basics/operators_part1.js
@@ -0,0 +1,53 @@
+// Increment & Decrement Operators 
+
+// x = 5;
+// // console.log(x++);
+// output1 = x++;
+// console.log(`Output1 = `+output1);  //5
+
+// y = 5;
+// // console.log(++y);
+// output2 = ++y;
+// console.log(`Output2 = `+output2);   //6
+
+// console.log(`x = `+x);    //6
+// console.log(`y = `,y);   //6
+
+// -------------------------------------------- //
+
+// let x = 5;
+// let y = 10;
+
+
+// x = x+y;  
+// //The above line is equivalent to x+=y
+
+// console.log(`x = `+x);  //15
+
+// -------------------------------------------- //
+
+// let i = 10;
+// console.log(i++);
+// console.log(i++);
+// console.log(i++);
+// console.log(i++);
+
+// -------------------------------------------- //
+
+// let m = 5; 
+// console.log(`m = `+m);
+// console.log(`n = `+(m++ + --m));
+
+// let o = 8;
+// console.log(o-- - --o);
+
+// let i = 2;
+// console.log(`Output = `+(i++ + ++i));
+// console.log(`i = `+ i);
+
+// -------------------------------------------- //
+
+
+
+
+
