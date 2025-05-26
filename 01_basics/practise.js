71a28926d5bfd5f9dff95bd17f8e85a389482106
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-05-26 13:03:32 +0530

    Added variables related notes

---
+++ 01_basics/practise.js
@@ -0,0 +1,10 @@
+var city = "Newyork";
+
+console.log(`city = ${city}`);
+
+if(true){
+    var city = "London";
+    console.log(`city = ${city}`);
+}
+
+console.log(`city = ${city}`);
---
+++ 01_basics/variables.js
@@ -0,0 +1,38 @@
+// let name = "Javascript"
+// let NAME = "JS"
+
+// console.log(name);
+// console.log(NAME);
+
+//-------------------------------------------------------------------//
+
+// Javascript is case-sensitive, Its is discovered by Brendon Eich in 1995
+// Variable declartion and assignment
+
+/* Note: You can NOT redelcare a block scoped variable in JAvascript however you can re-iniatize the value to it. 
+    Example: let, const 
+
+    Note: However in case of var - Global scropt variable, Both the redeclaration & re-initialization is allowed
+*/
+
+// var country = "India";
+// var country = "USA"
+
+// console.log(country);
+
+
+// let city = "Newyork";
+// city = "London";
+
+// const pi = 3.14;
+// // const pi = 4.14
+
+// console.log(city);
+// console.log(pi);
+
+//-------------------------------------------------------------------//
+
+
+
+
+
