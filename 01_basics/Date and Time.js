10b43638e77694f2af75ff3ea47c7e211a92f498
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-05-26 18:39:26 +0530

    Added Date and Time files

---
+++ 01_basics/Date and Time.js
@@ -0,0 +1,53 @@
+// Date object in JavaScript
+
+const currentDate = new Date();
+// console.log('Date in milliseconds:',currentDate);
+// console.log('toDateString:', currentDate.toDateString());
+// console.log('toLocaleDateString:', currentDate.toLocaleDateString());
+// console.log('toLocaleString:', currentDate.toLocaleString());
+// console.log('toLocaleString:', currentDate.toLocaleTimeString());
+// console.log('toLocaleString:', currentDate.toTimeString());
+
+
+// Date get methods
+
+// console.log('getDate:', currentDate.getDate());
+// console.log('getDay:', currentDate.getDay());
+// console.log('getFullYear:', currentDate.getFullYear());
+// console.log('getHours:', currentDate.getHours());
+// console.log('toISOgetMillisecondsString:', currentDate.getMilliseconds());
+// console.log('getMinutes:', currentDate.getMinutes());
+// console.log('getMonth:', currentDate.getMonth());
+// console.log('toISOString:', currentDate.getTime());
+// console.log('getTimezoneOffset:', currentDate.getTimezoneOffset());
+// console.log('getUTCDate:', currentDate.getUTCDate());
+// console.log('getUTCDay:', currentDate.getUTCDay());
+// console.log('getUTCFullYear:', currentDate.getUTCFullYear());
+
+// Timezone get methods
+// console.log('getUTCHours:', currentDate.getTime());
+// console.log('getTimezoneOffset:', currentDate.getTimezoneOffset());
+
+
+// Check if the time is am/pm
+
+// console.log('toLocaleString:', currentDate.toLocaleTimeString());
+// const am_pm = currentDate.getHours() >= 12 ? 'PM' : 'AM';
+// console.log('Current time is:', currentDate.getHours() + ':' + currentDate.getMinutes() + ' ' + am_pm); 
+
+// How to print the time in 12-hour format with AM/PM
+// var time = new Date();
+// console.log(
+//   time.toLocaleString('en-IN', { hour: 'numeric', hour12: true })
+// );  
+
+// const time_in_am_pm = time.toLocaleString('en-IN', { hour: 'numeric', hour12: true })
+// time_in_am_pm.toString().includes('AM') ? console.log((`AM`)) : console.log(`PM`);
+
+
+
+
+
+
+
+
