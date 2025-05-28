811a1412e64bc2dfc876df53cbbfea9b2e9011dc
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-05-28 20:04:31 +0530

    Updated string method .js file

--- 01_basics/string_method.js
+++ 01_basics/string_method.js
@@ -26,8 +26,74 @@
 // Replace all occurrences of a text using regex 
 
 
-
 //-------------------------------------------------------//
+
+// let courseName = "JavaScript";
+
+// console.log('charAt = '+courseName.charAt(0));
+// console.log(`charCodeAt = `+courseName.charCodeAt(0));
+// console.log(courseName.concat(' Programming'));
+
+// console.log(courseName);
+
+// let a1 = 5;
+// let a2 = "5";
+
+// console.log(a1 == a2);  //true - compares the value of content in both variables, Typecasting happens
+// console.log(a1 === a2); //compares both value and typeOf() of both the variables, Typecasting doesn't happen
+
+// let name1 = "JS";
+// let name2 = new String("JS");
+
+// console.log(name1 == name2);  //true - compares the value of content in both variables, Typecasting happens
+// console.log(name1 === name2); //compares both value and typeOf() of both the variables, Typecaseting doesn't happen
+
+// let city = "Delhi";
+
+// console.log(`${city} ends with 'i' >> `+city.endsWith(`i`));
+// console.log(`${city} starts with 'D' >> `+city.startsWith(`D`));
+
+// let fruitName = "Orange is a nice fruit, Its called Orange because that name suits properly";
+
+// console.log(fruitName.includes(`ange`));
+
+// console.log(`Index of method >> `+fruitName.indexOf('Orange'));
+
+// console.log(`Last index of >> `+fruitName.lastIndexOf('Orange'));
+
+// console.log(`Length of ${fruitName} = `+fruitName.length);
+
+// let courseName = "One Two Three Four Five";
+
+// console.log(`Pad End >> `+courseName.padEnd(25,'$'));
+// console.log(`Pad Start >> `+courseName.padStart(25,'-'));
+
+// console.log(`Replaced Text = `+courseName.replace(`Java`,'NodeJS'));
+// console.log(`Replaced Text = `+courseName.replaceAll(`Java`,'NodeJS'));
+// console.log(courseName);
+
+
+// console.log(courseName.split(' '));
+// console.log(courseName);
+
+// console.log(`Sliced Value > `+courseName.slice(3,13));
+// console.log(`Original Value > `+courseName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
