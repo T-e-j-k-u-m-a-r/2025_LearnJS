d1fac23fb2b21315a916b0d24f80c1d2506005f5
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-05-28 15:50:59 +0530

    Added strings method file

---
+++ 01_basics/string_method.js
@@ -0,0 +1,47 @@
+// String Methods: 
+// Replace all occurrences of a text using replaceAll method
+
+//-------------------------------------------------------//
+
+// let text = "Harry met Sally, When Harry met Sally, he was 18 years old.";
+
+// console.log(text.replace("Harry", "John")); 
+
+// console.log(text.replaceAll("Harry", "Joe") );
+
+//-------------------------------------------------------//
+
+// let text = "Harry met Sally, When Harry met Sally, he was 18 years old.";
+
+// let city = "Pune";
+
+// console.log(city.padStart(10,"$")); 
+// console.log(city.padEnd(10,"$"));
+
+// console.log(city);
+
+
+//-------------------------------------------------------//
+
+// Replace all occurrences of a text using regex 
+
+
+
+//-------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
