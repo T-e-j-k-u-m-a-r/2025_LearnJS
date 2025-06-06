e9dfe13fd88c74b375a3ac2166607d5b6eb1adcd
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-06-06 19:46:09 +0530

    Added strings file

---
+++ 01_basics/7thJune/strings.js
@@ -0,0 +1,48 @@
+// var version = "1.0";
+// console.log(version);
+
+// var name = "JavaScript";
+// console.log(name);
+
+// name = "Cypress";
+// console.log(name);
+
+var courseName = "JavaScript Programming";
+
+// console.log(courseName);
+
+// console.log(`charAt(4) : `+courseName.charAt(4));   //S
+// var updatedCourseName = courseName.concat(` From Scratch `);
+
+// console.log(courseName);
+// console.log(updatedCourseName);
+
+// console.log(`Does this string ends with ing character? `+courseName.endsWith(`ing`));   //true
+
+// console.log(`Does this string includes sub string of Programming word? `+courseName.includes(`Programming`));
+
+// console.log(`The index value of Programming word : `+courseName.indexOf(`Programming`));
+
+// console.log(`Pad End : `+courseName.padEnd(30,'😊'));
+// console.log(`Pad Start : `+courseName.padStart(30,'😊'));
+
+// console.log(courseName.repeat(4));
+
+// console.log(`subString value : `+courseName.substring(0,5));
+// console.log(courseName.length);
+
+// console.log(`subString value : `+courseName.slice(-4));
+// console.log(`subString value : `+courseName.slice(-8,19));
+
+
+
+
+
+
+
+
+
+
+
+
+
