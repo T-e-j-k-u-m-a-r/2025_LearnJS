27e424d5621b94561ec16ed9515bbf419c865b4e
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-06-12 13:48:13 +0530

    Added how to add 2 arrays concepts file

--- June/12th_June/Arrays/10.misc.js
+++ June/12th_June/Arrays/10.misc.js
@@ -1,34 +1,45 @@
-var numbers = [10,20,30,40,50,1,2,3,999];
-// console.log(numbers);
+// var numbers = [10,20,30,40,50,1,2,3,999];
+// // console.log(numbers);
 
-// Sort in Ascending order
-const result1 = numbers.sort(function cb(a,b){
+// // Sort in Ascending order
+// const result1 = numbers.sort(function cb(a,b){
 
-    return a - b;
+//     return a - b;
 
-})
+// })
 
-console.log(result1);
+// console.log(result1);
 
-//Find 1st Maximum value
-console.log((`1st Lowest value = `+result1.at(0)));
+// //Find 1st Maximum value
+// console.log((`1st Lowest value = `+result1.at(0)));
 
-//Find 2nd Maximmum value
-console.log((`2nd Lowest value = `+result1.at(1)));
+// //Find 2nd Maximmum value
+// console.log((`2nd Lowest value = `+result1.at(1)));
 
 
-// Sort in Desending order
-const result2 = numbers.sort(function cb(a,b){
+// // Sort in Desending order
+// const result2 = numbers.sort(function cb(a,b){
 
-    return b - a;
+//     return b - a;
 
-})
+// })
 
-console.log(result2);
+// console.log(result2);
 
-//Find 1st Maximum value
-console.log((`1st Highest value = `+result2.at(0)));
+// //Find 1st Maximum value
+// console.log((`1st Highest value = `+result2.at(0)));
 
-//Find 2nd Maximmum value
-console.log((`2nd Highest value = `+result2.at(1)));
+// //Find 2nd Maximmum value
+// console.log((`2nd Highest value = `+result2.at(1)));
+
+// var arr1 = [1,2,3];
+// var arr2 = [4,5,6];
+
+// var arr3 = arr1.concat(arr2);
+
+// console.log(arr3);
+// console.log(typeof(arr3));
+
+
+
 
